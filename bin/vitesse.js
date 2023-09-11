ea26772b2b364e09f4627b625d4781f6037a8af0
Lennart Järvinen <lennart.jaervinen@sitegeist.de>
2023-09-11 15:13:54 +0200

    [TASK] switch some comments

--- bin/vitesse.js
+++ bin/vitesse.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+// import packageInfo from '../package.json' with { type: "json" } // prepare, not stable support yet (with or assert)
 
 function start() {
-  // console.log('call func start')
+  console.log('🆅 🅸 🆃 🅴 🆂 🆂 🅴')
+  // console.log(packageInfo.version)
   return import('../dist/node/cli.js')
 }
 
